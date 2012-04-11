cb6c4152f925a9b0b754583cc96693abac63ebb6
Chris Hotchkiss <hotch11@gmail.com>
2012-04-11 19:03:13 +0100

    FizzBuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            for
+                (int x = 1; x <= 100; ++x)
+            {
+                if (x % 3 == 0 && x % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+
+                }
+
+                else
+                {
+                    if (x % 3 == 0)
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+
+                    if (x % 5 == 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+
+                    if (x % 3 != 0 && x % 5 != 0)
+                    {
+                        
+                        Console.WriteLine(x);
+                        
+                    }
+
+                        Thread.Sleep(100);
+
+                    
+                    
+                }
+            }
+        }
+    }
+}
